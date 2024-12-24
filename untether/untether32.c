5229ed0a9060f5a19bfa10467ce5ec3f9881edae
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2024-12-24 17:23:13 +0800

    Update untether32.c

--- untether/untether32.c
+++ untether/untether32.c
@@ -1,38 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <assert.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/syscall.h>
-#include <sys/kauth.h>
-#include <sys/mount.h>
-#include <sys/utsname.h>
-#include <spawn.h>
-#include <fcntl.h>
 
-#include <mach/mach.h>
-#include <IOKit/IOKitLib.h>
-
-#include "patchfinder.h"
 #include "sock_port_2_legacy/sockpuppet.h"
-
-#define TTB_SIZE            4096
-#define L1_SECT_S_BIT       (1 << 16)
-#define L1_SECT_PROTO       (1 << 1)        /* 0b10 */
-#define L1_SECT_AP_URW      (1 << 10) | (1 << 11)
-#define L1_SECT_APX         (1 << 15)
-#define L1_SECT_DEFPROT     (L1_SECT_AP_URW | L1_SECT_APX)
-#define L1_SECT_SORDER      (0)            /* 0b00, not cacheable, strongly ordered. */
-#define L1_SECT_DEFCACHE    (L1_SECT_SORDER)
-#define L1_PROTO_TTE(entry) (entry | L1_SECT_S_BIT | L1_SECT_DEFPROT | L1_SECT_DEFCACHE)
-#define L1_PAGE_PROTO       (1 << 0)
-#define L1_COARSE_PT        (0xFFFFFC00)
-#define PT_SIZE             256
-#define L2_PAGE_APX         (1 << 9)
-
-#define CHUNK_SIZE 0x800
 
 int main(void){
     mach_port_t tfp0;
