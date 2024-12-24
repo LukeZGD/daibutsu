64a87102f19cb3b74297bd1f99cf3321c3978ced
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2024-12-24 15:02:36 +0800

    remove everything, just run sp2l

--- untether/make8untether.sh
+++ untether/make8untether.sh
@@ -4,7 +4,7 @@ set -x
 
 sudo rm BUILD/daibutsu
 
-gcc -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.2.sdk -DUNTETHER -arch armv7 sock_port_2_legacy/*.c untether32.c patchfinder.c -o untether32 -framework IOKit -std=gnu99 -fno-stack-protector -Os -DIOS_VIII_UNTETHER
+gcc -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.2.sdk -arch armv7 sock_port_2_legacy/*.c untether32.c -o untether32 -framework IOKit -std=gnu99 -fno-stack-protector -Os
 
 strip untether32
 ldid -Sent.xml untether32
