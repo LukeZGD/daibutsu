8bb2518a7f206ca609b6a42aae2d653c3fa5e88c
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2024-12-27 11:56:11 +0800

    Update

--- untether/jailbreak.c
+++ untether/jailbreak.c
@@ -16,27 +16,9 @@
 #include <mach/mach.h>
 #include <IOKit/IOKitLib.h>
 
+#include "jailbreak.h"
 #include "sock_port_2_legacy/sockpuppet.h"
 #include "patchfinder.h"
-
-#define NEWFILE  (O_WRONLY|O_SYNC)
-#define CONSOLE "/dev/console"
-
-#define TTB_SIZE            4096
-#define L1_SECT_S_BIT       (1 << 16)
-#define L1_SECT_PROTO       (1 << 1)        /* 0b10 */
-#define L1_SECT_AP_URW      (1 << 10) | (1 << 11)
-#define L1_SECT_APX         (1 << 15)
-#define L1_SECT_DEFPROT     (L1_SECT_AP_URW | L1_SECT_APX)
-#define L1_SECT_SORDER      (0)            /* 0b00, not cacheable, strongly ordered. */
-#define L1_SECT_DEFCACHE    (L1_SECT_SORDER)
-#define L1_PROTO_TTE(entry) (entry | L1_SECT_S_BIT | L1_SECT_DEFPROT | L1_SECT_DEFCACHE)
-#define L1_PAGE_PROTO       (1 << 0)
-#define L1_COARSE_PT        (0xFFFFFC00)
-#define PT_SIZE             256
-#define L2_PAGE_APX         (1 << 9)
-
-#define CHUNK_SIZE 0x800
 
 struct utsname u = { 0 };
 
@@ -664,8 +646,8 @@ void unjail9(uint32_t kbase){
     
 }
 
+#ifdef UNTETHER
 void load_jb(void){
-    
     // remount rootfs
     printf("[*] remounting rootfs\n");
     char* nmr = strdup("/dev/disk0s1s1");
@@ -751,3 +733,4 @@ int main(void){
 
     return 0;
 }
+#endif
---
+++ untether/jailbreak.h
@@ -0,0 +1,21 @@
+#define NEWFILE  (O_WRONLY|O_SYNC)
+#define CONSOLE "/dev/console"
+
+#define TTB_SIZE            4096
+#define L1_SECT_S_BIT       (1 << 16)
+#define L1_SECT_PROTO       (1 << 1)        /* 0b10 */
+#define L1_SECT_AP_URW      (1 << 10) | (1 << 11)
+#define L1_SECT_APX         (1 << 15)
+#define L1_SECT_DEFPROT     (L1_SECT_AP_URW | L1_SECT_APX)
+#define L1_SECT_SORDER      (0)            /* 0b00, not cacheable, strongly ordered. */
+#define L1_SECT_DEFCACHE    (L1_SECT_SORDER)
+#define L1_PROTO_TTE(entry) (entry | L1_SECT_S_BIT | L1_SECT_DEFPROT | L1_SECT_DEFCACHE)
+#define L1_PAGE_PROTO       (1 << 0)
+#define L1_COARSE_PT        (0xFFFFFC00)
+#define PT_SIZE             256
+#define L2_PAGE_APX         (1 << 9)
+
+#define CHUNK_SIZE 0x800
+
+void unjail8(uint32_t kbase);
+void unjail9(uint32_t kbase);
